07969a149858ec00ca14c276726dd4584dacf369
minhvt61 <48672207+minh61@users.noreply.github.com>
2020-09-28 14:44:41 +0700

    Update profile.php

--- student/profile.php
+++ student/profile.php
@@ -10,49 +10,6 @@
     $id = (int)$_SESSION['id'];
     $data = get_member_by_id($id);
 
-    if (!empty($_POST['suathanhvien'])) {
-        $data['id'] = isset($_POST['id']) ? $_POST['id'] : '';
-
-        $data['Account'] = isset($_POST['account']) ? $_POST['account'] : '';
-
-        $data['Password'] = isset($_POST['password']) ? $_POST['password'] : '';
-        
-        $data['Fullname'] = isset($_POST['fullname']) ? $_POST['fullname'] : '';
-
-        $data['Title'] = isset($_POST['title']) ? $_POST['title'] : '';
-        
-        $data['Email'] = isset($_POST['email']) ? $_POST['email'] : '';
-
-        $data['Phone_number'] = isset($_POST['phone_number']) ? $_POST['phone_number'] : '';
-
-        $errors = array();
-        if (empty($data['Account'])){
-            $errors['Account'] = 'Chưa nhập tên đăng nhập';
-        }
-        if (empty($data['Password'])){
-            $errors['Password'] = 'Chưa nhập mật khẩu';
-        }
-        if (empty($data['Fullname'])){
-            $errors['Fullname'] = 'Chưa nhập họ và tên';
-        }
-        if (empty($data['Title'])){
-            $errors['Title'] = 'Chưa nhập chức vụ';
-        }
-        if (empty($data['Email'])){
-            $errors['Email'] = 'Chưa nhập email';
-        }
-        if (empty($data['Phone_number'])){
-            $errors['Phone_number'] = 'Chưa nhập số điện thoại';
-        }
-       
-        // Neu ko co loi thi insert
-        if (!$errors){
-            edit_member($data['id'],  $data['Account'], $data['Password'],$data['Fullname'], $data['Title'], $data['Email'],
-                $data['Phone_number']);
-            header("location:list_of_members.php");
-        }
-    }
-
 disconnect_db();
     
 ?>
@@ -87,7 +44,7 @@ disconnect_db();
         <br>
         <h2>Hồ sơ cá nhân </h2>
         <a href="list_of_members.php">Trở về</a> <br/> <br/>
-        <form method="post" action="profile.php?id=<?php echo $data['id']; ?>">
+        <form method="post" action="edit_member.php?id=<?php echo $data['id']; ?>">
             <table width="50%" border="1" cellspacing="0" cellpadding="10">
                 <tr>
                     <td>Tên đăng nhập</td>
@@ -98,7 +55,7 @@ disconnect_db();
                 </tr>
                 <td>Mật khẩu</td>
                     <td>
-                        <input type="password" name="password" value="<?php echo !empty($data['password']) ? $data['password'] : ''; ?>"/>
+                        <input type="password" name="password" value="<?php echo !empty($data['password']) ? $data['password'] : ''; ?>" disabled/>
                         <?php if (!empty($errors['account'])) echo $errors['account']; ?>
                     </td>
                 <tr>
@@ -118,21 +75,21 @@ disconnect_db();
                 <tr>
                     <td>Email</td>
                     <td>
-                        <input type="text" name="email" value="<?php echo !empty($data['email']) ? $data['email'] : ''; ?>"/>
+                        <input type="text" name="email" value="<?php echo !empty($data['email']) ? $data['email'] : ''; ?>" disabled/>
                         <?php if (!empty($errors['email'])) echo $errors['email']; ?>
                     </td>
                 </tr>
                 <tr>
                     <td>Số điện thoại</td>
                     <td>
-                        <input type="text" name="phone_number" value="<?php echo !empty($data['phone_number']) ? $data['phone_number'] : ''; ?>"/>
+                        <input type="text" name="phone_number" value="<?php echo !empty($data['phone_number']) ? $data['phone_number'] : ''; ?>" disabled/>
                         <?php if (!empty($errors['phone_number'])) echo $errors['phone_number']; ?>
                     </td>
                 </tr>
             </table>
             <br>
             <input type="hidden" name="id" value="<?php echo $data['id']; ?>"/>
-            <input type="submit" name="suathanhvien" value="Lưu"/>
+            <input type="submit" name="suathanhvien" value="Sửa"/>
         </form>
     </center>
     
